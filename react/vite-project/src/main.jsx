ab2fc4158850a5b8e575a287ebe16164e01191ac
Luisda1828 <LUIS.VILLEGASHERRERA@ucr.ac.cr>
2024-05-03 23:15:17 -0600

    Improvements in the events component.

--- react/vite-project/src/main.jsx
+++ react/vite-project/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import './main.css'
 import { NavDrawer } from './components/NavDrawer.jsx'
 import { MainSection } from './components/MainSection.jsx'
@@ -10,5 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <div className='overflow-x-hidden h-[100vh]'>
         <NavDrawer />
         <MainSection/>
+        <div></div>
     </div> 
 );
+
