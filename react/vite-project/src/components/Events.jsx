896332df2ac7d292c2db980267e5c381d0fa2969
Luisda1828 <LUIS.VILLEGASHERRERA@ucr.ac.cr>
2024-05-05 00:00:02 -0600

    Creation of the components of the Course Progress page

--- react/vite-project/src/components/Events.jsx
+++ react/vite-project/src/components/Events.jsx
@@ -2,16 +2,16 @@ import '../index.css';
 import Check from "../assets/evenready.svg";
 import Penta from "../assets/penta.svg";
 import Ramdom from "../assets/ramdomevent.png";
-
+import EventesMenu from "../assets/eventsMenu.svg";
 export function Eventos(titulo, text, image) {
     return (
-        <div className="mt-3 rounded-2xl min-h-32 ring-2 ring-white ml-[25%] mr-8 relative flex flex-col lg:flex-row justify-center items-center p-4 gap-1">
+        <div className="mt-3 rounded-2xl min-h-32 ring-2 ring-white ml-[25%] mr-8 relative flex flex-col lg:flex-row justify-center items-center px-4 gap-4 ">
             <div className="absolute h-8 w-8 bg-sky-600 border-b-2 border-l-2 border-white bottom-[32%] -left-[1.1rem] rotate-45"></div>
             <div className="absolute right-[127.5%] h-full flex flex-col">
                 <div className="relative flex justify-center">
-                    <div className="absolute size-[1.5rem] md:size-[1.8] bg-amber-500 rounded-full top-[3.5rem]"></div>
+                    <div className="absolute size-[1.5rem] md:size-[1.8] bg-amber-500 rounded-full top-[4rem]"></div>
                     <div className="absolute h-44 top-0 w-1 bg-amber-500 rounded-3xl"></div>
-                    <p className="absolute top-[2.5rem] left-5 text-white text-[1rem] xl:text-[2rem] text-nowrap">5:00pm</p>
+                    <p className="absolute top-[3rem] left-5 text-white text-[1rem] xl:text-[2rem] text-nowrap">5:00pm</p>
                 </div>
             </div>
 
@@ -22,21 +22,26 @@ export function Eventos(titulo, text, image) {
                 <img className='size-8' src={Penta} alt="" />
             </div>
 
-            <div className='flex flex-col text-white'>
-                <h1 className='text-[2rem]'>Lorem ipsum dolor sitet.</h1>
+            <div className='flex flex-col text-white '>
+                <div className='flex  max-w-[10rem] sm:max-w-full '>
+                <h1 className='text-[1.4rem]   lg:text-[2rem] whitespace-nowrap text-ellipsis overflow-hidden '>Lorem ipsum dolor sitet.</h1>
+                </div>
+              
                 <p className='hidden lg:block'>Lorem ipsum dolor sit amet consectetur. Feugiat hendrerit aliquet diam vulputate nibh. Adipisci dapibus. Elit commodo ullamcorper eu ac sit sit...</p>
             </div>
             
                
                 <div className=' flex w-full items-center justify-between gap-2 lg:hidden'>
-                    <button>
-                        <img src={Check} alt="" />
+                    <button className=''>
+                        <img className='size-8' src={Check} alt="" />
                     </button>
                     <img className='size-8' src={Penta} alt="" />
-                     <img src={Ramdom} alt="" />
+                     <img  className='size-12' src={Ramdom} alt="" />
                 </div>
           
                 <img className='hidden lg:block' src={Ramdom} alt="" />
+
+              {/*  <img  className='mb-12' src={EventesMenu} alt="" /> */}
 
         </div>
     );
--- react/vite-project/src/main.jsx
+++ react/vite-project/src/main.jsx
@@ -5,13 +5,14 @@ import { NavDrawer } from './components/NavDrawer.jsx'
 import { MainSection } from './components/MainSection.jsx'
 import { FormRegi } from './pages/FormRegi.jsx'
 import{AddEvent} from './components/AddEvent.jsx'
+import{Singup} from './pages/Singup.jsx'
+import { ProgressSection } from './components/ProgressSection.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     
     <div className='overflow-x-hidden h-[100vh]'>
-        
-
+       
         
     </div> 
 );
